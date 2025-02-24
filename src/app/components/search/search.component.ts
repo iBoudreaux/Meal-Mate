02d866ef7d16b6a36d5ac997ddc1b1fe7a975ee0
Isabella Boudreaux <iboudreaux@student.neumont.edu>
2025-02-23 22:36:50 -0700

    Added grabbing user input for search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-search',
   imports: [],
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+  searchParam: string = '';
+
+  onInputChange(event: Event) {
+    this.searchParam = (event.target as HTMLInputElement).value;
+  }
+
+  onSubmit() {
+    console.log('User input:', this.searchParam);
+    // call service
+  }
 
 }
